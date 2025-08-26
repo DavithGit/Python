2563a2e3b2cc4c7692e137e3c71b6469bcc78949
DavithGit <datwater2497@students.mchenry.edu>
2025-08-26 11:20:32 -0500

    Understanding data assignment

---
+++ UnderstandingData.py
@@ -0,0 +1,26 @@
+"""
+Copy the sample program into your Python editor.
+Run the program and observe the output.
+Change the variable values, test the program, and submit your file.
+"""
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 100)
+
+# Integer
+y = -40
+print("Integer:", y)
+
+# Float
+z = 22.22
+print("Float:", z)
+
+# String
+Tea = "Green"
+print("String:", Tea)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
