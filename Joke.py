fbf14bd0c92d8eca6d0e00688449799801535adf
DavithGit <datwater2497@students.mchenry.edu>
2025-08-26 10:58:52 -0500

    Python joke file

---
+++ Joke.py
@@ -0,0 +1,13 @@
+"""
+Create an original knock-knock joke or find one you like online and 
+use it when you write your program.
+Submit the .py file. Grading is complete/incomplete,
+and you may resubmit within a week of when the submission is graded.
+Include at least one comment (starts with # ) 
+to describe the program's purpose.
+"""
+#The purpose of the program is to print a joke using the print function
+print("There is a mommy tomato, daddy tomato, and baby tomato walking down the sidewalk")
+print("The baby tomato trips and falls behind")
+print("The daddy tomato steps on the baby tomato and says")
+print("Ketchup")
