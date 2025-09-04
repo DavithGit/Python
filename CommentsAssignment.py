c4b690ef637edd0e415968d1c17734b5dab2210d
DavithGit <datwater2497@students.mchenry.edu>
2025-09-04 10:55:30 -0500

    comments assignment upload

---
+++ CommentsAssignment.py
@@ -0,0 +1,26 @@
+""""
+Sample Python Program:
+tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+
+Directions:
+Type the above code into Visual Studio Code.
+Add a minimum of 4 comments to the above Python program to make it more understandable.
+ """
+#Define the size of the tank
+tank_size = 15
+#Define the total distance traveled
+distance = 600
+#Calculate the mpg, distance/ tank size
+mpg = distance / tank_size
+#print the mpg calculated
+print("Miles per gallon:", mpg)
+
+#comments are so useful!
+#ctrl + / toggles selected comments, which is also useful
+#remember to upload to git
