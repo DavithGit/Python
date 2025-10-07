e1ea3cde7977e811c2a2d1e40d4856590a5fa1e3
DavithGit <datwater2497@students.mchenry.edu>
2025-10-07 11:47:17 -0500

    uploads for modules assignment
    uploads for modules assignment

---
+++ math_operations/calculator.py
@@ -0,0 +1,9 @@
+
+#calculator.py is for 1.3 modules assignment
+
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
---
+++ math_operations/geometry.py
@@ -0,0 +1,14 @@
+def area_rectangle(a, b):
+    return a * b
+
+def perimiter(a, b):
+    return a + b
+
+def volume(a, b, c):
+    return a*b*c
+
+def area_triangle(a,b):
+    return a*b/2
+
+def area_circle(a):
+    return 3.14*a*a
